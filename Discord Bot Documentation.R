42dc81c6d331f684f9d29a19a35ba99c60a5cab1
Connor Nickol <cnickol26@gmail.com>
2021-12-09 20:15:28 -0500

    Add files via upload

---
+++ Discord Bot Documentation.R
@@ -0,0 +1,20 @@
+#Connor Nickol 12-8-21
+
+## Discord Bot Documentation
+
+#Reponds to words like help, confused, question, unsure, idk with
+# a multitude of responses
+#Connected to an external api where it gets random inspirational quotes
+
+## COMMANDS
+# $quote will have the bot responds with an inspirational quote
+# $new will add whatever follows to the response list for the help words
+# $del followed by a number will remove a response from the response list
+# $reponding false will turn off the bot responses
+# $reponding true will turn back on the bot responses
+
+## Hosted on REPLIT servers
+## Set up a ping system on a website called uptime robot that pings my code on REPLIT every 5 minutes
+## in order to keep the bot on constantly
+
+## I tried to use other servers but was unable to make anything else work properly 
